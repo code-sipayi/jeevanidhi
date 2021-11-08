9bb184a82fe8e47ff66aebcb947a93a9a2d51338
codesipayi <codesipayi@gmail.com>
2021-11-08 21:36:55 +0530

    youtube click count

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -17,6 +17,27 @@ function updateCounter(){
     });
 }
 
+function toggleYoutubeCount() {
+  var x = document.getElementById("counterYoutubeDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function updateYoutubeCounter(){
+  fetch(`https://api.countapi.xyz/hit/mysite.com/visits`)
+  .then(res => res.json())
+  .then(data=>{
+
+      counterYoutubeElement.innerHTML = data.value;
+      console.log(counterYoutubeElement);
+  });
+}
+
 updateCounter();
 
 counterElement = document.getElementsByClassName('webVisiterCount')[0];
+counterYoutubeElement = document.getElementsByClassName('youtubeCount')[0];
+
